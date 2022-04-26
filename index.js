4d94f67197f09662b84c4496785c5729eb10c875
Robert Gittings <rjgitting@gmail.com>
2022-04-26 19:43:28 -0400

    commit tree 1620d6d32365d0f04a85efef9ed261d06e1fcdd9

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function setBestCustomer () {
+    bestCustomer = `not bob`
+} 
+
+function upperCaseCustomerName (){
+    customerName = customerName.toUpperCase()
+}
+function overwriteBestCustomer (){
+    bestCustomer = (`maybe bob`)
+}
+
+const leastFavoriteCustomer = `gerald` 
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'gilles'
+}
+    
